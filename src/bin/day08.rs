d5d6ff4a6e63e372270d82e0247a6f753c9594a0
sts10 <sschlinkert@gmail.com>
2020-12-08 23:51:23 -0500

    in bnorrowing hell :(

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -17,22 +17,62 @@ fn main() {
     };
 
     loop {
-        let operation = split_and_vectorize(&instructions[my_head.current_position as usize], " ")
-            [0]
-        .to_string();
+        let cp = my_head.current_position as usize;
+        let operation = split_and_vectorize(&instructions[cp], " ")[0].to_string();
         let argument: isize =
             split_and_vectorize(&instructions[my_head.current_position as usize], " ")[1]
                 .parse()
                 .unwrap();
         println!("About to execute {} {}", operation, argument);
-        my_head = execute(operation, argument, my_head);
+        my_head = execute(operation, argument, &my_head);
         println!("Current position is {}", my_head.current_position);
         println!("Accumulator is {}", my_head.accumulator);
-        if my_head
-            .trail_of_positions
-            .contains(&(my_head.current_position as usize))
-        {
+        if does_this_loop_infinitely_if_next_instruction_is_switched(&instructions, &my_head) {
             break;
+        }
+    }
+}
+
+fn does_this_loop_infinitely_if_next_instruction_is_switched(
+    instructions: &[String],
+    my_head: &Head,
+) -> bool {
+    let mut new_head = my_head;
+    let mut first_time = true;
+    loop {
+        let operation = split_and_vectorize(&instructions[new_head.current_position as usize], " ")
+            [0]
+        .to_string();
+        let operation = if first_time && operation == "nop" {
+            first_time = false;
+            "jmp".to_string()
+        } else if first_time && operation == "jmp" {
+            first_time = false;
+            "nop".to_string()
+        } else {
+            first_time = false;
+            operation
+        };
+
+        let argument: isize =
+            split_and_vectorize(&instructions[new_head.current_position as usize], " ")[1]
+                .parse()
+                .unwrap();
+        println!("About to execute {} {}", operation, argument);
+        let mut new_head = execute(operation, argument, new_head);
+        println!("Current position is {}", new_head.current_position);
+        if new_head.current_position > instructions.len() as isize {
+            // terminates correctly
+            return false;
+        }
+        println!("Accumulator is {}", new_head.accumulator);
+        if new_head
+            .trail_of_positions
+            .contains(&(new_head.current_position as usize))
+        {
+            // we've looped around to this current positioan again,
+            // thus we are in an infinite loop
+            return true;
         }
     }
 }
@@ -54,6 +94,6 @@ fn execute(operation: String, argument: isize, old_head: Head) -> Head {
     Head {
         accumulator: new_accumulator,
         current_position: new_current_position,
-        trail_of_positions: new_trail_of_positions,
+        trail_of_positions: new_trail_of_positions.to_vec(),
     }
 }
