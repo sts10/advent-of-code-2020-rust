af2a26bdea383673b789d93834420c975e21401b
sts10 <sschlinkert@gmail.com>
2020-12-10 19:38:16 -0500

    naive attempt at d10 p2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,14 +1,57 @@
 use advent_of_code_2020::read_by_line;
 
 fn main() {
-    let file_name = "inputs/day10.txt";
-    let mut joltage_ratings: Vec<usize> = read_by_line(file_name).unwrap();
-    // let mut joltage_ratings: Vec<usize> = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+    // let file_name = "inputs/day10.txt";
+    // let mut joltage_ratings: Vec<usize> = read_by_line(file_name).unwrap();
+    let mut joltage_ratings: Vec<usize> = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
 
     joltage_ratings.sort();
+    // println!("Answer to part 1 is {}", solve_part_1(joltage_ratings));
+    println!(
+        "Answer to part 2 is {}",
+        count_higher_roads_not_taken(joltage_ratings)
+    );
+}
 
+fn count_higher_roads_not_taken(joltage_ratings: Vec<usize>) -> usize {
+    let mut vector_of_number_of_higher_forks_not_taken: Vec<usize> = vec![];
+    for (i, _joltage) in joltage_ratings.iter().enumerate() {
+        // let _next_three_joltages = vec![
+        //     joltage_ratings[i + 1],
+        //     joltage_ratings[i + 2],
+        //     joltage_ratings[i + 3],
+        // ];
+        if i + 4 > joltage_ratings.len() {
+            break;
+        }
+        let mut higher_forks_not_taken_this_time = 0;
+        // we're gonna take the lowest option [i+1] every time,
+        // but let's see if i+2 or i+3 qualify as valid jumps
+        if joltage_ratings[i + 2] - joltage_ratings[i] <= 3 {
+            higher_forks_not_taken_this_time += 1;
+        }
+        if joltage_ratings[i + 3] - joltage_ratings[i] <= 3 {
+            higher_forks_not_taken_this_time += 1;
+        }
+        vector_of_number_of_higher_forks_not_taken.push(higher_forks_not_taken_this_time + 1);
+    }
+    println!(
+        "completed vector_of_number_of_higher_forks_not_taken is {:?}",
+        vector_of_number_of_higher_forks_not_taken
+    );
+    // gonna screw with this part...
+    let mut product = 1;
+    for fork_count in vector_of_number_of_higher_forks_not_taken {
+        // product = product * fork_count;
+        if fork_count > 1 {
+            product = product * 2
+        }
+    }
+    product
+}
+
+fn solve_part_1(joltage_ratings: Vec<usize>) -> usize {
     let differences = make_vector_of_differences(joltage_ratings);
-
     let mut number_of_1_jumps = 0;
     let mut number_of_3_jumps = 0;
     for diff in differences {
@@ -18,12 +61,8 @@ fn main() {
             number_of_3_jumps += 1;
         }
     }
-    println!(
-        "So the answer to part 1 is {}",
-        number_of_1_jumps * number_of_3_jumps
-    );
+    number_of_1_jumps * number_of_3_jumps
 }
-
 fn make_vector_of_differences(joltage_ratings: Vec<usize>) -> Vec<usize> {
     // we want to pre-populate the Vector with the lowest joltage, since we start at 0
     // and since the inputted joltage_ratings Vector is already sorted,
